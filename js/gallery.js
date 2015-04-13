973470dc19251fb3279edbbf6ee0f37031940d28
Chris Miceli <git@miceli.net.au>
2015-04-12 19:57:01 -0700

    added the files for wedding

---
+++ js/gallery.js
@@ -0,0 +1,19 @@
+function showImage(e) {
+    e.preventDefault();
+    coverimage.setAttribute("src", this.getAttribute("href"));
+    coverimage.setAttribute("alt", this.querySelector("img").getAttribute("alt"));
+    cover.showModal();
+}
+
+document.getElementById("closecover").onclick = function() {
+    coverimage.setAttribute("src", "");
+    cover.close();
+}
+
+var imglinks = document.getElementById("thumbs").getElementsByTagName('a'),
+    cover = document.getElementById("cover"),
+    coverimage = cover.getElementsByTagName("img")[0];
+testdialog=document.createElement("dialog");
+testdialog.setAttribute("open", "");
+if (!testdialog.open) { dialogPolyfill.registerDialog(cover); }
+for (var i=0; i<imglinks.length; i++) { imglinks[i].onclick = showImage; }
